58a8f818909c8d990c43b66964093d0fa4145181
Matheus Teodoro <matheus.teodoro@farmaciasapp.com.br>
2023-05-21 21:13:52 -0300

    feat: creating api with two endpoints

---
+++ README.md
@@ -0,0 +1,51 @@
+<p align="center">
+
+  <h5 align="center">Serverless API</h5>
+
+  <p align="center">
+    Projeto desenvolvido por Matheus Teodoro
+    <br />
+    ·
+ 
+  </p>
+</p>
+
+# Sobre o projeto
+
+Nesse repositorio eu demonstro como usar o serverless framework para criar uma api serverless na aws, com express, api gateway e dynamo db.
+
+### Stack
+
+- [Node.js](https://nodejs.org/en/)
+
+# 🖥️ Para usar
+
+### Requisitos
+
+- **[Node.js](https://nodejs.org/en/)** instalado na máquina e de um gerenciador de dependencias (**[npm](https://www.npmjs.com/)** ou **[yarn](https://yarnpkg.com/)**)
+
+
+
+  # Clone este repositório
+  $ git clone https://github.com/matheusteodoro01/serverless-api.git
+
+  # Instalando as dependências
+  $ npm install
+
+  # Fazendo o deploy
+  $ npx serverless deploy
+  
+
+
+# Autor
+
+<a href="https://github.com/matheusteodoro01/">
+ <img src="https://avatars.githubusercontent.com/u/60862196?v=4" width="70px;" alt=""/>
+ <br />
+ <sub><b>Matheus Teodoro</b></sub>
+</a>
+
+Feito com ❤️ por Matheus Teodoro :wave::wave: Entre em contato!🚀
+
+<a href="https://www.linkedin.com/in/matheus-teodoro-7bb92818a/">
+</a>
---
+++ handler.js
@@ -0,0 +1,62 @@
+import AWS from "aws-sdk";
+import express from "express";
+import serverless from "serverless-http";
+
+const app = express();
+const USERS_TABLE = process.env.USERS_TABLE;
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+
+app.use(express.json());
+
+app.get("/users/:userId", async (req, res) => {
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: req.params.userId,
+    },
+  };
+
+  try {
+    const { Item } = await dynamoDbClient.get(params).promise();
+    if (Item) {
+      const { userId, name, email } = Item;
+      return res.json({ userId, name, email });
+    } else {
+      return res
+        .status(404)
+        .json({ error: 'Could not find user with provided "userId"' });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Could not retreive user" });
+  }
+});
+
+app.post("/users", async (req, res) => {
+  const { userId, name, email } = req.body;
+
+  const params = {
+    TableName: USERS_TABLE,
+    Item: {
+      userId,
+      name,
+      email,
+    },
+  };
+
+  try {
+    await dynamoDbClient.put(params).promise();
+    return res.json({ userId, name, email });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Could not create user" });
+  }
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+export const handler = serverless(app);
